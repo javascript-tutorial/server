51788d5324b1213a6532d00521b53ea7faa6bdf5
Ilya Kantor <iliakan@users.noreply.github.com>
2018-08-24 22:55:35 +0300

    Update momentWithLocale.js

--- modules/momentWithLocale.js
+++ modules/momentWithLocale.js
@@ -1,3 +1,5 @@
-require('moment/locale/' + process.env.NODE_LANG);
+if (process.env.NODE_LANG !== 'en') {
+  require('moment/locale/' + process.env.NODE_LANG);
+}
 
 module.exports = require('moment');
