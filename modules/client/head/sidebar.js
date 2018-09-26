50f524c9bdc20c70a32290be6fda8b825695e9b5
Ilya Kantor <iliakan@gmail.com>
2018-09-26 20:35:35 +0300

    sidebar highlight

--- modules/client/head/sidebar.js
+++ modules/client/head/sidebar.js
@@ -3,6 +3,8 @@ document.addEventListener('click', onClick);
 
 document.addEventListener('keydown', onKeyDown);
 
+initSidebarHighlight();
+
 function toggle() {
 
   let pageWrapper = document.querySelector('.page-wrapper');
@@ -11,11 +13,15 @@ function toggle() {
 
   pageWrapper && pageWrapper.classList.toggle('page-wrapper_sidebar_on');
 
-  if (document.querySelector('.page').classList.contains('page_sidebar_on')) {
-    delete localStorage.noSidebar;
-  } else {
-    localStorage.noSidebar = 1;
-  }
+  window.acceptGdpr(accepted => {
+    if (document.querySelector('.page').classList.contains('page_sidebar_on')) {
+      delete localStorage.noSidebar;
+    } else {
+      if (accepted) {
+        localStorage.noSidebar = 1;
+      }
+    }
+  });
 
 }
 
@@ -45,3 +51,42 @@ function onKeyDown(event) {
   event.preventDefault();
 
 }
+
+function initSidebarHighlight() {
+
+  function highlight() {
+
+    let current = document.getElementsByClassName('sidebar__navigation-link_active');
+    if (current[0]) current[0].classList.remove('sidebar__navigation-link_active');
+
+    //debugger;
+    let h2s = document.getElementsByTagName('h2');
+    let i;
+    for (i = 0; i < h2s.length; i++) {
+      let h2 = h2s[i];
+      // first in-page header
+      // >1, because when visiting http://javascript.local/native-prototypes#native-prototype-change,
+      // top may be 0.375 or kind of...
+      if (h2.getBoundingClientRect().top > 1) break;
+    }
+    i--; // we need the one before it (currently reading)
+
+    if (i >= 0) {
+      let href = h2s[i].firstElementChild && h2s[i].firstElementChild.getAttribute('href');
+      let li = document.querySelector('.sidebar__navigation-link a[href="' + href + '"]');
+      if (href && li) {
+        li.classList.add('sidebar__navigation-link_active');
+      }
+    }
+
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    highlight();
+
+    window.addEventListener('scroll', highlight);
+  });
+
+
+}
+
